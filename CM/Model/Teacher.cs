7cf9bea138cc102eb5e03059352f356d63caa8c2
Haessen <blackandwhite56@163.com>
2020-12-24 13:04:27 +0800

    ignore

---
+++ CM/Model/Teacher.cs
@@ -0,0 +1,46 @@
+﻿/*
+ | CLR版本:  4.0.30319.42000
+ |
+ | 作　　者: Haessen
+ |
+ | 創建時間: 2020/12/7 11:32:49
+ |
+ | 描　　述: 請对Teacher類進行簡單的描述........
+ |
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.Model
+{
+    public class Teacher
+    {
+        /*------------------------------------------ Fields ------------------------------------------*/
+        #region Fields
+
+        #endregion
+        /*---------------------------------------- Properties ----------------------------------------*/
+        #region Properties
+
+        #endregion
+        /*----------------------------------- Dependency Properties ----------------------------------*/
+        #region Dependency Properties
+
+        #endregion
+        /*--------------------------------------- Constructors ---------------------------------------*/
+        #region Constructors
+
+        #endregion
+        /*-------------------------------------- Public Methods --------------------------------------*/
+        #region Public Methods
+
+        #endregion
+        /*-------------------------------------- Private Methods -------------------------------------*/
+        #region Private Methods
+
+        #endregion
+    }
+}
